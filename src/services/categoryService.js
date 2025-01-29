57198416ccfda44ff4c9da2ce2b4bb48714e0be9
choua <chouavang2022@gmail.com>
2025-01-29 19:15:46 +0700

    add category

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -2,14 +2,25 @@ import Category from "../models/category.js";
 
 // Create a new category
 export const serviceCategory = {
-  createCategory: async (productData) => {
-    const category = new Category(productData);
+  createCategory: async (categoryData) => {
+    const category = new Category(categoryData);
     await category.save();
     return category;
   },
   findAll: async () => {
     const categories = await Category.find();
     return categories;
+  },
+  deleteCategory: async (id) => {
+    const category = await Category.findByIdAndDelete(id);
+  },
+  updateCategory: async (id, categoryData) => {
+    const category = await Category.findByIdAndUpdate(id, categoryData, { new: true });
+    return category;
+  },
+  findCategoryById: async (id) => {
+    const category = await Category.findById(id);
+    return category;
   },
 };
 
