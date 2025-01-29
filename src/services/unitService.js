d2b736a9fa5afdce206c85d2d73b6f2d6f2fc410
choua <chouavang2022@gmail.com>
2025-01-29 19:06:42 +0700

    add unitservice

--- src/services/unitService.js
+++ src/services/unitService.js
@@ -2,8 +2,8 @@ import Unit from "../models/unit.js";
 
 // Create a new unit
  const serviceUnit = {
-    createUnit: async (productData) => {
-        const unit = new Unit(productData);
+    createUnit: async (unitData) => {
+        const unit = new Unit(unitData);
         await unit.save();
         return unit;
     },
@@ -11,7 +11,24 @@ import Unit from "../models/unit.js";
     findAlls: async () => {
         const units = await Unit.find();
         return units;
+    }, 
+    // Delete a unit
+    deleteUnit: async (id) => {
+         await Unit.findByIdAndDelete(id);
     },
+    // Update a unit
+     updateUnit: async (id, unitData) => {
+        const unit = await Unit.findByIdAndUpdate(id, unitData, { new: true });
+        if(!unit){
+            return res.status(404).json({message:"NOT FOUND"})
+        }
+        return unit;
+    },
+    // Find a unit by id
+    findUnitById: async (id) => {
+        const unit = await Unit.findById(id);
+        return unit;
+    }
 }
 
 export default serviceUnit;
